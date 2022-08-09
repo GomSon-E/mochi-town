a899d6dfa367ca4ae5af2cf493361386746a1f60
GomSon-E <gomsonixx@gmail.com>
2022-08-10 02:47:33 +0900

    Complete main.html - box04

---
+++ js/main_data.js
@@ -0,0 +1,186 @@
+window.data = {
+  "allTitle" : [
+    "꿀잠보장! 안고자면 세상포근", "파격할인! 있을 때 데려가세요!", "모찌 인형 쿠션 under35cm 이하", "꺄 귀여워! under20cm이하 애착인형"
+  ],
+  "productsByTitle" : {
+    "꿀잠보장! 안고자면 세상포근" : [
+      {
+        "id": "0",
+        "name": "goodnight01",
+        "product": "모찌타운 꿀잠 잘 시간 꿈나라로 파자마 룩 잠만자 바디필로우",
+        "del": "59,000원",
+        "ins": "37,400원",
+        "recommend": true
+      },
+      {
+        "id": "1",
+        "name": "goodnight02",
+        "product": "모찌타운 봉봉 카피바라 바디필로우 모찌 쿠션",
+        "del": "47,700원",
+        "ins": "27,900원",
+        "recommend": true
+      },
+      {
+        "id": "2",
+        "name": "goodnight03",
+        "product": "물개가되고픈 여름 냉감바디필로우 인형 65cm",
+        "del": "25,000원",
+        "ins": "25,000원",
+        "recommend": true
+      },
+      {
+        "id": "3",
+        "name": "goodnight04",
+        "product": "잠만자 바디필로우 대형모찌모찌인형 70cm (안대증정)",
+        "del": "32,000원",
+        "ins": "25,600원",
+        "recommend": false
+      },
+      {
+        "id": "4",
+        "name": "goodnight05",
+        "product": "해달이 되고픈 바디필로우 대형모찌모찌인형 65cm",
+        "del": "25,000원",
+        "ins": "19,500원",
+        "recommend": true
+      },
+      {
+        "id": "5",
+        "name": "goodnight06",
+        "product": "모찌모찌 허스키 레오 쿠션인형 L",
+        "del": "27,000원",
+        "ins": "21,900원",
+        "recommend": true
+      },
+      {
+        "id": "6",
+        "name": "goodnight07",
+        "product": "모찌모찌 까망 고양이 쿠션인형 L",
+        "del": "27,000원",
+        "ins": "21,900원",
+        "recommend": false
+      },
+      {
+        "id": "7",
+        "name": "goodnight08",
+        "product": "모찌모찌 베르 고양이 쿠션인형 L",
+        "del": "27,000원",
+        "ins": "21,900원",
+        "recommend": false
+      },
+      {
+        "id": "8",
+        "name": "goodnight09",
+        "product": "모찌모찌 시바 쿠 쿠션인형 L",
+        "del": "27,000원",
+        "ins": "21,900원",
+        "recommend": true
+      }      
+    ],
+  
+    "파격할인! 있을 때 데려가세요!" : [
+      {
+        "id": "9",
+        "name": "sale01",
+        "product": "모찌타운 꿀잠 잘 시간 꿈나라로 파자마 룩 잠만자 바디필로우",
+        "del": "59,000원",
+        "ins": "37,400원",
+        "recommend": true
+      },  
+      {
+        "id": "10",
+        "name": "sale02",
+        "product": "말랑말랑 눈사람 실리콘 LED 무드등 (USB충전)",
+        "del": "16,900원",
+        "ins": "16,900원",
+        "recommend": true
+      },  
+      {
+        "id": "11",
+        "name": "sale03",
+        "product": "모찌타운 포근포근 서있는 인형 뚠뚜니 4종 30cm",
+        "del": "23,000원",
+        "ins": "16,500원",
+        "recommend": true
+      },  
+      {
+        "id": "12",
+        "name": "sale04",
+        "product": "모찌모찌 얼굴쿠션",
+        "del": "20,000원",
+        "ins": "14,000원",
+        "recommend": true
+      }
+    ],
+  
+    "모찌 인형 쿠션 under35cm 이하" : [
+      {
+        "id": "13",
+        "name": "under35cm01",
+        "product": "모찌모찌 베르 고양이 쿠션인형 S",
+        "del": "11,000원",
+        "ins": "9,900원",
+        "recommend": false
+      },
+      {
+        "id": "14",
+        "name": "under35cm02",
+        "product": "모찌타운 포근포근 서있는 인형 뚠뚜니 4종 30cm",
+        "del": "23,000원",
+        "ins": "16,500원",
+        "recommend": true
+      },
+      {
+        "id": "15",
+        "name": "under35cm03",
+        "product": "모찌모찌 베르 고양이 쿠션인형 M",
+        "del": "20,000원",
+        "ins": "17,900원",
+        "recommend": false
+      },
+      {
+        "id": "16",
+        "name": "under35cm04",
+        "product": "모찌모찌 까망 고양이 쿠션인형 M",
+        "del": "20,000원",
+        "ins": "17,900원",
+        "recommend": false
+      },
+      {
+        "id": "17",
+        "name": "under35cm05",
+        "product": "모찌모찌 시바 쿠 쿠션인형 M",
+        "del": "20,000원",
+        "ins": "17,900원",
+        "recommend": true
+      }
+    ],
+  
+    "꺄 귀여워! under20cm이하 애착인형" : [
+      {
+        "id": "19",
+        "name": "under20cm01",
+        "product": "모찌타운 플러피 쿼카 가방고리 키링 브라운 크림",
+        "del": "12,500원",
+        "ins": "9,900원",
+        "recommend": true
+      },
+      {
+        "id": "20",
+        "name": "under20cm02",
+        "product": "모찌모찌 스트레스볼 스퀴시 스트레스해소 손장난감",
+        "del": "8,000원",
+        "ins": "6,500원",
+        "recommend": true
+      },
+      {
+        "id": "21",
+        "name": "under20cm03",
+        "product": "모찌모찌 시바 쿠 쿠션인형 S",
+        "del": "11,000원",
+        "ins": "9,900원",
+        "recommend": true
+      }
+    ]
+  }
+}
---
+++ js/main_index.js
@@ -0,0 +1,77 @@
+(function() {
+  function App() {
+    const [title, setTitle] = React.useState("꿀잠보장! 안고자면 세상포근")
+    const [products, setProducts] = React.useState(window.data.productsByTitle["꿀잠보장! 안고자면 세상포근"])
+    const titles = window.data.allTitle
+
+    function handleTitleChange(selectedTitle) {
+      const ableProducts = window.data.productsByTitle[selectedTitle]
+      setProducts(ableProducts)
+      setTitle(selectedTitle)
+    }
+
+    return (
+      <>
+        <Title title={title} titles={titles} handleTitleChange={handleTitleChange} />
+        <Product products={products} />
+      </>
+    )
+
+    // Title
+    function Title(props) {
+      function optionTitle() {
+        return props.titles.map((item, index) => {return <option key={index} value={item}>{item}</option>})
+      }
+
+      function onTitleChange(e) {
+        props.handleTitleChange(e.target.value)
+      }
+
+      return (
+        <select id="title" defaultValue={props.title} onChange={onTitleChange}>
+          {optionTitle()}
+        </select>     
+      )
+    }
+
+    // Product
+    function Product(props) {
+      function isRecommended(recommend) {
+        if (recommend == true) {
+          return <span>추천</span>
+        } 
+      }      
+
+      function showProducts() {
+        return props.products.map((item, index) => {return (
+          <figure>
+            <div className="img_div">
+              <img src={`../image/${item.name}.jpg`} alt={item.product} />
+              <p>
+                <button>장바구니</button>
+                <button>관심상품</button>
+                <button>좋아요</button>
+              </p>
+            </div>                
+            <figcaption>{item.product}</figcaption>
+            <del>{item.del}</del>
+            <ins>{item.ins}</ins>
+            {isRecommended(item.recommend)}
+          </figure>
+        )})
+      }
+
+      return (
+        <div className="content_wrap">
+          {showProducts()}
+        </div>
+      )
+    }
+  }
+
+  // export
+  const box04 = document.querySelector("#box04")
+  ReactDOM.render(
+    <App />, box04
+  )
+})()
